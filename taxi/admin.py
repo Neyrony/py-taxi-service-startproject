f931864cfd292e8b3a37a0338dfbf023028cf949
Neyrony <sserebriy@gmail.com>
2026-06-25 18:03:39 +0300

    added migrations

--- taxi/admin.py
+++ taxi/admin.py
@@ -10,13 +10,18 @@ class DriverAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         ("Additional info", {"fields": ("license_number",)}),
     )
-    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("license_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
+    )
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
+    list_display = ("model", "manufacturer")
     search_fields = ("model",)
     list_filter = ("manufacturer",)
 
-admin.site.register(Manufacturer)
 
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ("name", "country")
